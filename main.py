010fd257a5157272f38e00957ca1721a88487369
dattaswapnanil <dattaswapnanil@gmail.com>
2025-10-07 12:04:02 +0530

    st.secret

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ from datetime import datetime
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+DB_HOST = st.secrets["DB_HOST"]
+DB_PORT = st.secrets["DB_PORT"]
+DB_USER = st.secrets["DB_USER"]
+DB_PASSWORD = st.secrets["DB_PASSWORD"]
+DB_NAME = st.secrets["DB_NAME"]
 
 # Page configuration
 st.set_page_config(
